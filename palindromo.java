94647e5284402423f43896e8a8b6db44dae3e71c
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-08 17:01:43 -0300

    palindromo.java funcionando

---
+++ palindromo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class palindromo {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+
+		while (!input.equals("FIM")) {
+			System.out.println(isPalindromo(input) ? "SIM" : "NAO");
+			input = sc.nextLine();
+		}
+
+		sc.close();
+	}
+
+	public static boolean isPalindromo(String str)
+	{
+		int esq = 0;
+		int dir = str.length() - 1;
+		boolean ret = true;
+
+		while (esq < dir)
+			if (str.charAt(esq++) != str.charAt(dir--))
+				ret = false;
+		return ret;
+	}
+}
