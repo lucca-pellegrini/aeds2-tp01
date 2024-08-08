0e28c28fcdcc439db02b63cfa9cec51b6f26bc40
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-08 17:48:16 -0300

    palindromo.c: use somente wchar_t com um buffer fixo

--- palindromo.c
+++ palindromo.c
@@ -5,13 +5,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 // Configurações de localização, para ler caracteres não-ASCII corretamente.
 #include <locale.h>
 
 // Inclusão de tipo de dado wide-character (veja `man wchar.h` no Linux).
 #include <wchar.h>
+
+#define TAM_BUF (1 << 10) // Tamanho do buffer de texto.
 
 bool is_palindromo(wchar_t *str, int len);
 int remover_newline(wchar_t *restrict str);
@@ -21,35 +22,18 @@ int main(void)
 	// Ajusta o locale do programa para português em UTF-8.
 	setlocale(LC_ALL, "pt_BR.UTF-8");
 
-	char *str = NULL; // String de entrada.
-	wchar_t *wstr = NULL; // String em tipo “wide-character”.
-	size_t bufsz = 0; // Tamanho do buffer da string.
+	wchar_t input[TAM_BUF]; // Buffer fixo para a string de entrada.
 	int len; // Quantidade de caracteres na string.
 
-	getline(&str, &bufsz, stdin); // Lê uma linha da input.
+	while (fgetws(input, TAM_BUF, stdin) != NULL) {
+		len = remover_newline(input); // Remove os newlines.
 
-	// Converte a string normal para a string “wide”.
-	len = mbstowcs(NULL, str, 0) + 1; // Computa o tamanho da nova string.
-	wstr = malloc(len * sizeof(wchar_t)); // Aloca memória para ela.
-	mbstowcs(wstr, str, len); // Faz a conversão.
+		if (wcscmp(input, L"FIM") == 0)
+			break;
 
-	len = remover_newline(wstr); // Remove os newlines.
-
-	while (wcscmp(wstr, L"FIM")) {
-		puts(is_palindromo(wstr, len) ? "SIM" : "NAO");
-
-		getline(&str, &bufsz, stdin); // Lê a próxima linha.
-
-		// Faz a conversão da nova linha e remove os newlines.
-		len = mbstowcs(NULL, str, 0) + 1;
-		wstr = realloc(wstr, len * sizeof(wchar_t));
-		mbstowcs(wstr, str, len);
-		len = remover_newline(wstr);
+		puts(is_palindromo(input, len) ? "SIM" : "NAO");
 	}
 
-	// Libera a memória e termina.
-	free(str);
-	free(wstr);
 	return 0;
 }
 
@@ -58,7 +42,7 @@ bool is_palindromo(wchar_t *str, int len)
 	bool resposta = true; // Valor de retorno.
 	// Índices dos limites esquerdo e direito da string.
 	int esq = 0;
-	int dir = len - 1; 
+	int dir = len - 1;
 
 	// Itera sobre a string dos dois lados comparando caracteres um por um.
 	while (esq < dir)
