829a76bbc2c27e37be10150167b44e2331678506
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-08 18:20:05 -0300

    palindromo.c: use `wscanf` para simplificar o programa

--- palindromo.c
+++ palindromo.c
@@ -4,7 +4,6 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 // Configurações de localização, para ler caracteres não-ASCII corretamente.
 #include <locale.h>
@@ -12,37 +11,33 @@
 // Inclusão de tipo de dado wide-character (veja `man wchar.h` no Linux).
 #include <wchar.h>
 
-#define TAM_BUF (1 << 10) // Tamanho do buffer de texto.
+#define TAM_BUF (1 << 10) // Tamanho do buffer de texto (1KB).
 
-bool is_palindromo(wchar_t *str, int len);
-int remover_newline(wchar_t *restrict str);
+bool is_palindromo(wchar_t *str);
 
 int main(void)
 {
-	// Ajusta o locale do programa para português em UTF-8.
+	wchar_t input[TAM_BUF]; // Buffer fixo para a string de entrada.
+
+	// Ajusta o locale do programa para português em UTF-8 (somente Linux).
 	setlocale(LC_ALL, "pt_BR.UTF-8");
 
-	wchar_t input[TAM_BUF]; // Buffer fixo para a string de entrada.
-	int len; // Quantidade de caracteres na string.
+	wscanf(L"%l[^\n\r]s", input); // Lê uma linha da entrada.
 
-	while (fgetws(input, TAM_BUF, stdin) != NULL) {
-		len = remover_newline(input); // Remove os newlines.
-
-		if (wcscmp(input, L"FIM") == 0)
-			break;
-
-		puts(is_palindromo(input, len) ? "SIM" : "NAO");
+	while (wcscmp(input, L"FIM") != 0) {
+		puts(is_palindromo(input) ? "SIM" : "NAO"); // Printa resposta.
+		wscanf(L" %l[^\n\r]s", input); // Lê a próxima linha.
 	}
 
 	return 0;
 }
 
-bool is_palindromo(wchar_t *str, int len)
+bool is_palindromo(wchar_t *str)
 {
 	bool resposta = true; // Valor de retorno.
 	// Índices dos limites esquerdo e direito da string.
 	int esq = 0;
-	int dir = len - 1;
+	int dir = wcslen(str) - 1;
 
 	// Itera sobre a string dos dois lados comparando caracteres um por um.
 	while (esq < dir)
@@ -50,18 +45,4 @@ bool is_palindromo(wchar_t *str, int len)
 			resposta = false;
 
 	return resposta;
-}
-
-// Remove da string quaisquer delimitadores de linha (`\n` e `\r`).
-int remover_newline(wchar_t *restrict str)
-{
-	int len = 0;
-
-	// Itera sobre a string e copia os caracteres que não são newlines.
-	for (int i = 0; str[i] != L'\0'; ++i)
-		if (str[i] != L'\n' && str[i] != L'\r')
-			str[len++] = str[i];
-	str[len] = L'\0'; // Insere o terminador na posição final.
-
-	return len;
 }
