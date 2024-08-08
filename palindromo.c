2fa58ce8ac6c94489fbb3c727afb540cf0f957f1
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-08 18:38:33 -0300

    palindromo.c: remova excesso de comentários
    Também ajuste o tamanho do buffer de entrada, e conserte alguns
    comentários.

--- palindromo.c
+++ palindromo.c
@@ -1,28 +1,21 @@
-/* Nesse programa, foi necessário usar o tipo de dado “wide-character”
- * (wchar_t), uma vez que o arquivo de entrada usado pelo Verde tem caracteres
- * compostos por mais de 8 bits.*/
-
+#include <locale.h>
 #include <stdbool.h>
 #include <stdio.h>
-
-// Configurações de localização, para ler caracteres não-ASCII corretamente.
-#include <locale.h>
-
-// Inclusão de tipo de dado wide-character (veja `man wchar.h` no Linux).
 #include <wchar.h>
 
-#define TAM_BUF (1 << 10) // Tamanho do buffer de texto (1KB).
+#define TAM_BUF (1 << 9) // Tamanho do buffer fixo de texto (512 caracteres).
 
 bool is_palindromo(wchar_t *str);
 
 int main(void)
 {
-	wchar_t input[TAM_BUF]; // Buffer fixo para a string de entrada.
+	wchar_t input[TAM_BUF]; // Buffer fixo de texto para a entrada.
 
 	// Ajusta o locale do programa para português em UTF-8 (somente Linux).
 	setlocale(LC_ALL, "pt_BR.UTF-8");
 
-	wscanf(L"%l[^\n\r]s", input); // Lê uma linha da entrada.
+	// Lê uma linha da entrada. Ignora newlines.
+	wscanf(L"%l[^\n\r]s", input);
 
 	while (wcscmp(input, L"FIM") != 0) {
 		puts(is_palindromo(input) ? "SIM" : "NAO"); // Printa resposta.
