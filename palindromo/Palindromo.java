bb0a7f316a60e3a72d8888d9a15c6fd2a31745f8
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-10 01:44:01 -0300

    Palindromo: formata corretamente a implementação Java

--- palindromo/Palindromo.java
+++ palindromo/Palindromo.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
 
 public class Palindromo {
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
-		while (!input.equals("FIM")) {
-			System.out.println(isPalindromo(input) ? "SIM" : "NAO");
-			input = sc.nextLine();
-		}
+        while (!input.equals("FIM")) {
+            System.out.println(isPalindromo(input) ? "SIM" : "NAO");
+            input = sc.nextLine();
+        }
 
-		sc.close();
-	}
+        sc.close();
+    }
 
-	public static boolean isPalindromo(String str)
-	{
-		int esq = 0;
-		int dir = str.length() - 1;
-		boolean ret = true;
+    public static boolean isPalindromo(String str) {
+        int esq = 0;
+        int dir = str.length() - 1;
+        boolean ret = true;
 
-		while (esq < dir)
-			if (str.charAt(esq++) != str.charAt(dir--))
-				ret = false;
-		return ret;
-	}
+        while (esq < dir)
+            if (str.charAt(esq++) != str.charAt(dir--))
+                ret = false;
+        return ret;
+    }
 }
