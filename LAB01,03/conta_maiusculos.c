98f6e04e6eecc6f66779f42e927e96a1b749739d
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-16 18:24:59 -0300

    L03: melhora resultado evitando o uso de wcs???()
    Essa implementação, claramente, não é mais correta do que a feita em
    a5c02af. A diferença no resultado reportado pelo Verde se deve ao fato
    de que a implementação anterior (que atende às exigências da linguagem)
    não ser aceita pelo verde, presumidamente porque não aceita nenhuma das
    configurações de localização que testei. Deve ser possível determinar a
    localização padrão fazendo o upload do seguinte código:
    
    	int main(void) {puts(setlocale(LC_ALL, "")); return 0;}
    
    De qualuer forma, a implementação anterior não funcionava, e sempre
    causava no verde um *timeout* ao tentar executar wcslen() em qualquer
    texto que contém um ‘�’. Esse commit faz manualmente a verificação de
    caractere maiúsculo, e não usa nenhuma função de <wctype.h>,
    determinando manualmente o momento em que se deve interromper o laço de
    repetição ao comparar o caractere no índice atual com L'\0'.
    
    Ainda se trata de uma taxa de acerto de apenas 5,4% no Verde, mas isso
    se deve ao fato de que o programa crasha ao encontrar o primeiro
    caractere ‘�’, o que comprova que, com a atual configuração de
    localização, o sistema corretor do verde não consegue sequer indexar a
    string wchar_t *texto, comprovando que se trata de uma limitação da
    plataforma.
    
    Testarei com outras configurações de localização.

--- LAB01,03/conta_maiusculos.c
+++ LAB01,03/conta_maiusculos.c
@@ -1,7 +1,6 @@
 #include <locale.h>
 #include <stdio.h>
 #include <wchar.h>
-#include <wctype.h>
 
 #define TAM_BUF (1 << 9) // Tamanho do buffer fixo de texto (512 caracteres).
 
@@ -12,14 +11,15 @@ int main(void)
 	wchar_t input[TAM_BUF]; // Buffer fixo de texto para a entrada.
 
 	// Ajusta o locale do programa para português em UTF-8 (somente Linux).
-	setlocale(LC_ALL, "C.UTF-8");
+	if (!setlocale(LC_ALL, "C.UTF-8")) {
+		fprintf(stderr, "Erro ao configurar locale.\n");
+		return 1;
+	}
 
 	// Lê uma linha da entrada. Ignora newlines.
-	wscanf(L"%l[^\n\r]s", input);
-
-	while (wcscmp(input, L"FIM") != 0) {
+	while (wscanf(L" %l[^\n\r]s", input) == 1 &&
+	       wcscmp(input, L"FIM") != 0) {
 		printf("%u\n", num_maiusculas(input)); // Printa resposta.
-		wscanf(L" %l[^\n\r]s", input); // Lê a próxima linha.
 	}
 
 	return 0;
@@ -30,8 +30,9 @@ unsigned num_maiusculas(const wchar_t *texto)
 	unsigned count = 0; // Valor de retorno.
 
 	// Usamos `size_t` pois o compilador reclama com `int`.
-	for (size_t i = 0; i < wcslen(texto); ++i)
-		if (iswupper(texto[i])) // Verifica se é maiúscula.
+	for (size_t i = 0; texto[i] != L'\0'; ++i)
+		// Verifica se é ASCII e maiúscula.
+		if (texto[i] >= 'A' && texto[i] <= 'Z')
 			++count;
 
 	return count;
