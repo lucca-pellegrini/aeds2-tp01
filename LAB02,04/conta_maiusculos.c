ccb4a4ef535dc8ccc92ef1bd554172afa96d832c
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-18 21:37:57 -0300

    L04: termina a questão
    Veja c8b9cc9.

--- LAB02,04/conta_maiusculos.c
+++ LAB02,04/conta_maiusculos.c
@@ -1,40 +1,34 @@
-#include <locale.h>
+#include <ctype.h>
 #include <stdio.h>
-#include <wchar.h>
-#include <wctype.h>
+#include <string.h>
 
-#define TAM_BUF 256
+#define TAM_BUF (1 << 10)
 
-unsigned num_maiusculas(const wchar_t *texto, size_t tam);
+unsigned num_maiusculas(const char *texto, size_t tam);
 
 int main(void)
 {
-	wchar_t input[TAM_BUF]; // Buffer fixo de texto para a entrada.
+	char input[TAM_BUF]; // Buffer fixo de texto para a entrada.
 
-	// Ajusta o locale do programa para português em UTF-8 (somente Linux).
-	if (!setlocale(LC_ALL, "C.UTF-8")) {
-		fprintf(stderr, "Erro ao configurar locale.\n");
-		return 1;
-	}
-
-	// Lê uma linha da entrada. Ignora newlines.
-	while (scanf(" %255l[^\n\r]s", input) != EOF &&
-	       wcscmp(input, L"FIM") != 0) {
-		printf("%u\n", num_maiusculas(input, wcslen(input)));
+	// Lê uma linha da entrada até achar “FIM”. Ignora newlines.
+	while (scanf(" %1023[^\n\r]s", input) != EOF &&
+	       strcmp(input, "FIM") != 0) {
+		printf("%u\n", num_maiusculas(input, strlen(input)));
 	}
 
 	return 0;
 }
 
-unsigned num_maiusculas(const wchar_t *texto, size_t tam)
+unsigned num_maiusculas(const char *texto, size_t tam)
 {
 	unsigned ret; // Valor de retorno.
 
 	if (tam == 0) {
 		ret = 0;
 	} else {
+		// Chamada recursiva na substring, excluindo o primeiro item.
 		ret = num_maiusculas(texto + 1, tam - 1);
-		if (iswupper(*texto))
+		if (isupper(*texto)) // Verifica se é maiúscula.
 			++ret;
 	}
 
