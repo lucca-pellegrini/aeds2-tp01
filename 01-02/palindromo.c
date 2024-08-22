ffb43e3ec53120a10cd558efbef1d871a6521355
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-22 14:02:05 -0300

    01-02,10-11: elimina wcscmp() e wcslen()
    Veja 91ac036.

--- 01-02/palindromo.c
+++ 01-02/palindromo.c
@@ -6,6 +6,8 @@
 #define TAM_BUF (1 << 9) // Tamanho do buffer fixo de texto (512 caracteres).
 
 bool is_palindromo(wchar_t *str);
+unsigned wcslen2(wchar_t *str);
+int wcscmp2(wchar_t *str, wchar_t *cmp);
 
 int main(void)
 {
@@ -17,7 +19,7 @@ int main(void)
 	// Lê uma linha da entrada. Ignora newlines.
 	wscanf(L"%l[^\n\r]s", input);
 
-	while (wcscmp(input, L"FIM") != 0) {
+	while (wcscmp2(input, L"FIM") != 0) {
 		puts(is_palindromo(input) ? "SIM" : "NAO"); // Printa resposta.
 		wscanf(L" %l[^\n\r]s", input); // Lê a próxima linha.
 	}
@@ -30,7 +32,7 @@ bool is_palindromo(wchar_t *str)
 	bool resposta = true; // Valor de retorno.
 	// Índices dos limites esquerdo e direito da string.
 	int esq = 0;
-	int dir = wcslen(str) - 1;
+	int dir = wcslen2(str) - 1;
 
 	// Itera sobre a string dos dois lados comparando caracteres um por um.
 	while (esq < dir)
@@ -38,4 +40,25 @@ bool is_palindromo(wchar_t *str)
 			resposta = false;
 
 	return resposta;
+}
+
+unsigned wcslen2(wchar_t *str) // Pois não se usa wcslen() nos TPs.
+{
+	unsigned count;
+	for (count = 0; str[count] != L'\0'; ++count)
+		; // Loop vazio.
+	return count;
+}
+
+int wcscmp2(wchar_t *str, wchar_t *cmp)
+{
+	int ret = 0;
+	int min_len = (wcslen2(str) > wcslen2(cmp)) ? wcslen2(cmp) :
+						      wcslen2(str);
+
+	for (int i = min_len - 1; i > 0; --i)
+		if (str[i] != cmp[i])
+			ret = str[i] - cmp[i];
+
+	return ret;
 }
--- 10-11/palindromo.c
+++ 10-11/palindromo.c
@@ -6,6 +6,8 @@
 #define TAM_BUF (1 << 9) // Tamanho do buffer fixo de texto (512 caracteres).
 
 bool is_palindromo(wchar_t *str, int esq, int dir);
+unsigned wcslen2(wchar_t *str);
+int wcscmp2(wchar_t *str, wchar_t *cmp);
 
 int main(void)
 {
@@ -17,9 +19,9 @@ int main(void)
 	// Lê uma linha da entrada. Ignora newlines.
 	wscanf(L"%l[^\n\r]s", input);
 
-	while (wcscmp(input, L"FIM") != 0) {
-		puts(is_palindromo(input, 0, wcslen(input) - 1) ? "SIM" :
-								  "NAO");
+	while (wcscmp2(input, L"FIM") != 0) {
+		puts(is_palindromo(input, 0, wcslen2(input) - 1) ? "SIM" :
+								   "NAO");
 		wscanf(L" %l[^\n\r]s", input); // Lê a próxima linha.
 	}
 
@@ -38,4 +40,25 @@ bool is_palindromo(wchar_t *str, int esq, int dir)
 		resposta = is_palindromo(str, esq + 1, dir - 1);
 
 	return resposta;
+}
+
+unsigned wcslen2(wchar_t *str) // Pois não se usa wcslen() nos TPs.
+{
+	unsigned count;
+	for (count = 0; str[count] != L'\0'; ++count)
+		; // Loop vazio.
+	return count;
+}
+
+int wcscmp2(wchar_t *str, wchar_t *cmp)
+{
+	int ret = 0;
+	int min_len = (wcslen2(str) > wcslen2(cmp)) ? wcslen2(cmp) :
+						      wcslen2(str);
+
+	for (int i = min_len - 1; i > 0; --i)
+		if (str[i] != cmp[i])
+			ret = str[i] - cmp[i];
+
+	return ret;
 }
