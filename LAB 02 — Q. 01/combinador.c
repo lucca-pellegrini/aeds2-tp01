91ac0364222afe1b6c71255355bb2944d2595c67
Lucca Pellegrini <632054@sga.pucminas.br>
2024-08-22 13:11:47 -0300

    L02Q01: adequa questão às exigências do TP.
    Não se deve usar strlen, strtok, strcmp, etc. E, em Java, não se deve
    usar StringBuilder, e só podemos usar, da classe String, os métodos
    .charAt() e .length().

--- LAB 02 — Q. 01/combinador.c
+++ LAB 02 — Q. 01/combinador.c
@@ -4,7 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Funções principais.
 void combina(char *restrict const input, char *restrict output);
+
+// Reimplementações (simplificadas) de funções padrão.
+void remove_newline(char *str); // Simula parte de `strtok()`.
+unsigned strlen2(char *str);
 
 int main(void)
 {
@@ -14,8 +19,9 @@ int main(void)
 
 	// Aloca memória e lê uma linha da entrada.
 	while (getline(&texto, &cap, stdin) != -1) {
+		remove_newline(texto);
 		res = realloc(res, cap); // Aloca memória para o resultado.
-		combina(texto, res); // Combina as strings.
+		combina(texto, res); // Combina as substrings no buffer `res`.
 		puts(res); // Mostra o resultado.
 	}
 
@@ -26,13 +32,16 @@ int main(void)
 	return 0;
 }
 
-/* É proveitoso ler a documentação de strtok() para compreender como a string
- * de input é dividida: <https://man.archlinux.org/man/strtok.3>. */
 void combina(char *restrict const input, char *restrict output)
 {
 	int tam1, tam2, tam_max; // Tamanhos das substrings de entrada.
-	char *restrict str1 = strtok(input, " \r\n"); // Primeira substring.
-	char *restrict str2 = strtok(NULL, " \r\n"); // Substring após espaço.
+	char *restrict str1 = input; // Primeira substring.
+	char *restrict str2 = input; // Substring após espaço.
+
+	// Move o segundo ponteiro até a segunda substring e delimita a primeira.
+	while (*str2 != ' ')
+		++str2;
+	*str2++ = '\0'; // Delimita primeira, incrementa segunda.
 
 	// Verificação de erros.
 	if (str1 == NULL || str2 == NULL || str1 == str2) {
@@ -42,7 +51,7 @@ void combina(char *restrict const input, char *restrict output)
 
 	// Computa os tamanhos.
 	tam1 = str2 - str1 - 1;
-	tam2 = strlen(str2);
+	tam2 = strlen2(str2);
 	tam_max = (tam1 > tam2) ? tam1 : tam2;
 
 	/* Itera sobre as substrings, alternando entre elas e copiando para a
@@ -54,4 +63,19 @@ void combina(char *restrict const input, char *restrict output)
 			*output++ = str2[i];
 	}
 	*output = '\0';
+}
+
+void remove_newline(char *strp) // Pois não se usa strtok() nos TPs.
+{
+	while (*strp != '\n' && *strp != '\r')
+		++strp;
+	*strp = '\0';
+}
+
+unsigned strlen2(char *str) // Pois não se usa strlen() nos TPs.
+{
+	unsigned count;
+	for (count = 0; str[count] != '\0'; ++count)
+		; // Loop vazio.
+	return count;
 }
