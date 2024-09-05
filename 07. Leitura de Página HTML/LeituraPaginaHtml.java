112bde2371ff5f91366a3c202a02cfb642c8c75d
Lucca Pellegrini <632054@sga.pucminas.br>
2024-09-05 16:33:18 -0300

    07: reimplementa tudo, termina questão
    Abandonamos o uso do HashMap, pois é muito difícil usar uma String na
    codificação correta como chave. Por isso, torna-se necessário
    manualmente verificar cada caractere possível, então a função contadora
    foi reescrita, usando um arranjo de inteiros para os contadores de cada
    vogal, e contadores individuais para consoantes, line breaks, e tabelas.
    Adicionalmente, implementa a lógica necessária para desconsiderar
    quaisquer vogais e consoantes nas tags HTML de line break e tabela.
    Finalmente, forçamos o uso de uma stream de entrada com a codificação de
    caracteres ISO-8859-1, pois, aparentemente, é o que o verde espera, como
    podemos evidenciar com o comando `file`:
    
    	$ file -i pub.out
    	pub.out: text/html; charset=iso-8859-1

--- 07. Leitura de Página HTML/LeituraPaginaHtml.java
+++ 07. Leitura de Página HTML/LeituraPaginaHtml.java
@@ -1,126 +1,207 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.nio.charset.Charset;
 
-public class LeituraPaginaHtml
-{
-    public static final String VOGAIS = "aeiouáéíóúàèìòùãõâêîôû";
-    public static final String CONSOANTES = "bcdfghjklmnpqrstvwxyz";
-
+public class LeituraPaginaHtml {
     // É necessário explicitar os `throws` porque se não o compilador reclama.
-    public static void main(String[] args)
-        throws URISyntaxException, IOException
-    {
-        Scanner sc = new Scanner(System.in);
-        String nome = sc.nextLine();
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        PrintStream out = new PrintStream(System.out, true, "ISO-8859-1");
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in, Charset.forName("UTF-8")));
+        String nome = br.readLine();
 
         while (strcmp(nome, "FIM") != 0) {
-            String url = sc.nextLine();
+            String url = br.readLine();
 
             // Baixa a página especificada no `url`.
             String resposta = baixaPagina(url);
 
-            // Computa e printa os resultados.
-            Map<String, Integer> resultados = conta(resposta.toLowerCase());
-            System.out.println(resultStr(resultados, nome));
+            out.println(contaElementosHtml(resposta) + " " + nome);
 
-            nome = sc.nextLine();
+            nome = br.readLine();
         }
-
-        sc.close();
     }
 
-    public static String baixaPagina(String input)
-        throws URISyntaxException, IOException
-    {
+    public static String baixaPagina(String input) throws URISyntaxException, IOException {
         URL url = new URI(input).toURL(); // Instancia um objeto URL.
 
         // Instancia um objeto para ler a resposta.
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(url.openStream()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 
         String resposta = new String();
         String linha;
 
         // Itera sobre a resposta no objeto, linha por linha, e apende à saída.
         while ((linha = br.readLine()) != null)
-            resposta += linha;
+            resposta += linha + "\n";
 
         return resposta;
     }
 
-    public static Map<String, Integer> conta(String html)
-    {
-        // Contadores locais.
-        int consoantes = 0, breaks = 0, tabelas = 0;
+    public static boolean isConsoante(char c) {
+        boolean ret = false;
+        if (c >= 'b' && c <= 'd' || c >= 'f' && c <= 'h' || c >= 'j' && c <= 'n' ||
+                c >= 'p' && c <= 't' || c >= 'v' && c <= 'z') {
+            ret = true;
+        }
+        return ret;
+    }
 
-        // Cria uma tabela hash para contar todos os itens. Será retornada.
-        Map<String, Integer> countMap = new HashMap<String, Integer>();
+    public static String contaElementosHtml(String html) {
+        int[] vog = new int[23]; // Contadores de vogais.
+        int con = 0; // Contador de consoantes.
+        int brk = 0; // Contador de line breaks `<br>`.
+        int tbl = 0; // Contador de tabelas `<table>`
 
-        // Inicializa todos os itens na tabela como 0.
-        for (int i = 0; i < VOGAIS.length(); ++i)
-            countMap.put("" + VOGAIS.charAt(i), 0);
+        for (int i = 0; i < html.length(); i++) {
+            char charAtual = html.charAt(i);
+            int intAtual = (int) charAtual; // charAtual codificado como int.
 
-        // Itera sobre o HTML caractere por caractere.
-        for (int i = 0; i < html.length(); ++i) {
-            String c = "" + html.charAt(i);
+            // Começamos contando as vogais simples.
+            switch (charAtual) {
+                case 'a':
+                    ++vog[1];
+                    break;
+                case 'e':
+                    ++vog[2];
+                    break;
+                case 'i':
+                    ++vog[3];
+                    break;
+                case 'o':
+                    ++vog[4];
+                    break;
+                case 'u':
+                    ++vog[5];
+                    break;
+            }
 
-            // Insere vogais na tabela hash.
-            if (VOGAIS.contains(c))
-                countMap.put(c, countMap.get(c) + 1);
+            // Depois, contamos as vogais acentuadas. Em cada seção desse
+            // switch-case, um acento é considerado, e cada vogal é testada em
+            // ordem alfabética (i.e.: á é í ó ú à è ì ò ù...).
+            switch (intAtual) {
+                // Acento agudo: ´
+                case 225:
+                    ++vog[6];
+                    break;
+                case 233:
+                    ++vog[7];
+                    break;
+                case 237:
+                    ++vog[8];
+                    break;
+                case 243:
+                    ++vog[9];
+                    break;
+                case 250:
+                    ++vog[10];
+                    break;
 
-            // Conta consoantes.
-            else if (CONSOANTES.contains(c))
-                ++consoantes;
+                // Crase: `
+                case 224:
+                    ++vog[11];
+                    break;
+                case 232:
+                    ++vog[12];
+                    break;
+                case 236:
+                    ++vog[13];
+                    break;
+                case 242:
+                    ++vog[14];
+                    break;
+                case 249:
+                    ++vog[15];
+                    break;
+
+                // Til: ~
+                case 227:
+                    ++vog[16];
+                    break;
+                case 245:
+                    ++vog[17];
+                    break;
+
+                // Acento circunflexo: ^
+                case 226:
+                    ++vog[18];
+                    break;
+                case 234:
+                    ++vog[19];
+                    break;
+                case 238:
+                    ++vog[20];
+                    break;
+                case 244:
+                    ++vog[21];
+                    break;
+                case 251:
+                    ++vog[22];
+                    break;
+            }
+
+            // Testamos se é consoante.
+            if (isConsoante(charAtual))
+                ++con;
+
+            // Se encontrar uma abertura de tag (`<`), testamos manualmente se
+            // é line break ou tabela.
+            if (charAtual == '<') {
+                if (i + 3 < html.length() && html.charAt(i + 1) == 'b' &&
+                        html.charAt(i + 2) == 'r' && html.charAt(i + 3) == '>') {
+                    ++brk;
+                }
+
+                if (i + 6 < html.length() && html.charAt(i + 1) == 't' &&
+                        html.charAt(i + 2) == 'a' && html.charAt(i + 3) == 'b' &&
+                        html.charAt(i + 4) == 'l' && html.charAt(i + 5) == 'e' &&
+                        html.charAt(i + 6) == '>') {
+                    ++tbl;
+                }
+            }
         }
 
-        // Usa Regular Expressions para encontrar as tabelas e os breaks.
-        Matcher breakMatcher = Pattern.compile("<br>").matcher(html);
-        Matcher tabelaMatcher = Pattern.compile("<table>").matcher(html);
-        while (breakMatcher.find())
-            ++breaks;
-        while (tabelaMatcher.find())
-            ++tabelas;
+        // Decrementamos os contadores de vogais e consoantes para
+        // desconsiderar caracteres que estão nas tags que contamos.
+        con -= 2 * brk + 3 * tbl; // Desconsidera b, r, t, b, e l nas tags.
+        vog[1] -= 1 * tbl; // Desconsidera os `a`s nas `<table>`s.
+        vog[2] -= 1 * tbl; // Desconsidera os `e`s nas `<table>`s.
 
-        // Insere os resultados na tabela.
-        countMap.put("consoante", consoantes);
-        countMap.put("<br>", breaks);
-        countMap.put("<table>", tabelas);
-
-        return countMap;
+        return ("a(" + vog[1] + ") "
+                + "e(" + vog[2] + ") "
+                + "i(" + vog[3] + ") "
+                + "o(" + vog[4] + ") "
+                + "u(" + vog[5] + ") "
+                + "á(" + vog[6] + ") "
+                + "é(" + vog[7] + ") "
+                + "í(" + vog[8] + ") "
+                + "ó(" + vog[9] + ") "
+                + "ú(" + vog[10] + ") "
+                + "à(" + vog[11] + ") "
+                + "è(" + vog[12] + ") "
+                + "ì(" + vog[13] + ") "
+                + "ò(" + vog[14] + ") "
+                + "ù(" + vog[15] + ") "
+                + "ã(" + vog[16] + ") "
+                + "õ(" + vog[17] + ") "
+                + "â(" + vog[18] + ") "
+                + "ê(" + vog[19] + ") "
+                + "î(" + vog[20] + ") "
+                + "ô(" + vog[21] + ") "
+                + "û(" + vog[22] + ") "
+                + "consoante(" + con + ") "
+                + "<br>(" + brk + ") "
+                + "<table>(" + tbl + ")");
     }
 
-    public static String resultStr(Map<String, Integer> map, String nome)
-    {
-        // Buffer contendo os caracteres que formarão a String.
-        String buf = new String();
-
-        // Adiciona as vogais.
-        for (char v : VOGAIS.toCharArray())
-            buf += v + "(" + map.get("" + v) + ") ";
-
-        // Adiciona os outros contadores.
-        buf += "consoante(" + map.get("consoante") + ") ";
-        buf += "<br>(" + map.get("<br>") + ") ";
-        buf += "<table>(" + map.get("<table>") + ") ";
-        buf += nome;
-
-        return buf;
-    }
-
-    public static int strcmp(String str, String cmp)
-    {
+    public static int strcmp(String str, String cmp) {
         int ret = 0;
-        int min_len = (str.length() > cmp.length()) ? cmp.length() :
-                                                      str.length();
+        int min_len = (str.length() > cmp.length()) ? cmp.length() : str.length();
 
         for (int i = min_len - 1; i >= 0; --i)
             if (str.charAt(i) != cmp.charAt(i))
